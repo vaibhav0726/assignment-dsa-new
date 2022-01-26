5e256f00c2245aeaae1a5cf7b82221ec0028152f
Vaibhav Varshney <vaibhavvarshney008@gmail.com>
2022-01-26 21:29:46 +0530

    Calculating number of digits in the given number by using recursion.

---
+++ src/recursion/Number_of_digits.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Number_of_digits {
+
+    public static int find(int n){
+        if(n/10==0){
+            return 1;
+        }
+        return 1+find(n/10);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println(find(n));
+    }
+}
